0ed9d78600a4ae43ddf58b8a7012407c077c719f
Himanshu Pal <himanshupal2412@gmail.com>
2026-07-19 10:50:26 +0530

    Update CORS to dynamically allow all origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,7 +22,9 @@ const PORT = process.env.PORT || 5000;
 // Security Middlewares
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
   credentials: true
 }));
 
